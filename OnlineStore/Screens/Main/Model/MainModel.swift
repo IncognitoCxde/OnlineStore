3fe5a23d6c63e58c256f8f6dc8b6a4afa3be9762
IncognitoCxde <benissaeaston@gmail.com>
2025-09-28 16:16:20 +0500

    MVVM prep commit

---
+++ OnlineStore/Screens/Main/Model/MainModel.swift
@@ -0,0 +1,7 @@
+//
+//  MainModel.swift
+//  OnlineStore
+//
+//  Created by iMacbook on 9/28/25.
+//
+
---
+++ OnlineStore/Screens/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  OnlineStore
+//
+//  Created by iMacbook on 9/28/25.
+//
+
+import UIKit
+
+class MainViewModel {
+    
+}
