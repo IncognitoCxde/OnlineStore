87b3e1c736b20e2c5604a7cf864fb7d88d90e01b
IncognitoCxde <benissaeaston@gmail.com>
2025-10-02 20:09:30 +0500

    DesignSystem prep commit

--- OnlineStore/Screens/TabBar/Destinations/Profile/ProfileViewController.swift
+++ OnlineStore/Screens/TabBar/Destinations/Profile/ProfileViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import DesignSystem
 
 class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGrey
+        view.backgroundColor = AppColors.lightGrey
     }
 
 }
