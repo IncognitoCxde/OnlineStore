5d965fabad5f8c64ae39da83d73dfdfb737f6074
IncognitoCxde <benissaeaston@gmail.com>
2025-10-02 20:04:14 +0500

    DesignSystem commit

--- OnlineStore/Screens/Main/Model/MainModel.swift
+++
@@ -1,7 +0,0 @@
-//
-//  MainModel.swift
-//  OnlineStore
-//
-//  Created by iMacbook on 9/28/25.
-//
-
--- OnlineStore/Screens/Main/ViewModel/MainViewController.swift
+++ OnlineStore/Screens/Main/ViewModel/MainViewController.swift
@@ -6,15 +6,15 @@
 //
 
 import UIKit
+import SnapKit
+import DesignSystem
 
 class MainViewController: UIViewController {
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGrey
         
     }
-
+    
 }
 
