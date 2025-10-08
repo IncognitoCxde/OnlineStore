8296518f1a7111eb2a9c4a721783aaaa3a4636f7
IncognitoCxde <benissaeaston@gmail.com>
2025-10-08 13:48:14 +0500

    fix commit

--- OnlineStore/Screens/Main/ViewModel/MainViewController.swift
+++ OnlineStore/Screens/Main/ViewModel/MainViewController.swift
@@ -172,8 +172,8 @@ class MainViewController: UIViewController {
                 
                 return section
                 
+                
             }
-            
         }
     }
     
