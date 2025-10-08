3ee3feb48f558adcaa364554b7c4dc3bd03094b9
IncognitoCxde <benissaeaston@gmail.com>
2025-10-08 10:45:16 +0500

    prep for merge commit

--- OnlineStore/Screens/Main/MainViewController.swift
+++ OnlineStore/Screens/Main/MainViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  OnlineStore
-//
-//  Created by iMacbook on 9/27/25.
-//
 
 import UIKit
-import DesignSystem
 
 class MainViewController: UIViewController {
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = AppColors.lightGrey
         
     }
 
