60dcc95c248649597d3e5ec45fe23c84c3761363
IncognitoCxde <benissaeaston@gmail.com>
2025-10-11 18:07:31 +0500

    .

--- OnlineStore/Screens/Main/ViewModel/MainViewModel.swift
+++ OnlineStore/Screens/Main/ViewModel/MainViewModel.swift
@@ -22,17 +22,6 @@ final class MainViewModel {
         CollectionCategory(name: "Shoes")
     ]
     
-//    func loadMockData() {
-//        products = [
-//            Product(id: 1, title: "Monitor HD 22'inch Display", images: ["Monitor"], price: 299.99),
-//            Product(id: 2, title: "Playstation 4 - SSD 128GB", images: ["Playstation"], price: 499.99),
-//            Product(id: 3, title: "Airpods pro", images: ["Headphones"], price: 199.99),
-//            Product(id: 4, title: "Macbook Pro M2 13'inch", images: ["Mac"], price: 1999.99)
-//        ]
-//        
-//        onDataUpdated?()
-//    }
-    
     var onDataUpdated: (() -> Void)?
     
     func fetchData(completion: @escaping () -> Void) {
