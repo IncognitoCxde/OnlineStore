9b00bf688fe1244849830dff26a41ebd4fb2652b
IncognitoCxde <benissaeaston@gmail.com>
2025-10-20 18:54:57 +0500

    cart Vc commit

--- OnlineStore/Screens/Cart/ViewModel/CartViewController.swift
+++ OnlineStore/Screens/Cart/ViewModel/CartViewController.swift
@@ -19,7 +19,17 @@ class CartViewController: UIViewController {
         button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
         return button
     }()
-    
+        
+    private let deliveryLabel = UILabel()
+    private let locationButton = UIButton(type: .system)
+
+    private let summaryView = UIView()
+    private let summaryTitleLabel = UILabel()
+    private let totalTitleLabel = UILabel()
+    private let totalValueLabel = UILabel()
+    private let paymentButton = UIButton(type: .system)
+
+
     let tableView = UITableView()
     
     // MARK: - ViewDidLoad
@@ -36,6 +46,33 @@ class CartViewController: UIViewController {
         setUpTitleLabel()
         setUpBackButton()
         setUpConstraints()
+        setUpTableView()
+        setUpDeliverySection()
+        setUpSummaryView()
+        setUpSummaryViewConstraints()
+    }
+    
+    func setUpDeliverySection() {
+        view.addSubview(deliveryLabel)
+        view.addSubview(locationButton)
+        
+        deliveryLabel.text = "Delivery to"
+        deliveryLabel.textColor = AppColors.arsenicDark
+        deliveryLabel.font = AppFont.regular18pt(size: 16)
+        
+        locationButton.setTitle("Salatiga City, Central Java ▾", for: .normal)
+        locationButton.setTitleColor(AppColors.arsenicDark, for: .normal)
+        locationButton.titleLabel?.font = AppFont.semiBold_18pt(size: 16)
+        
+        deliveryLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        locationButton.snp.makeConstraints { make in
+            make.centerY.equalTo(deliveryLabel)
+            make.trailing.equalToSuperview().inset(20)
+        }
     }
     
     func setUpBackButton() {
@@ -68,10 +105,83 @@ class CartViewController: UIViewController {
     }
     
     func setUpTableView() {
+        view.addSubview(tableView)
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(CartTableViewCell.self, forCellReuseIdentifier: "CartTableViewCell")
         tableView.dataSource = self
         tableView.delegate = self
         
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(deliveryLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(summaryView.snp.top)
+        }
     }
+    
+    // MARK: - Summary View (payment & total)
+    
+    func setUpSummaryView() {
+        view.addSubview(summaryView)
+        summaryView.backgroundColor = .white
+        summaryView.layer.cornerRadius = 20
+        summaryView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        summaryView.layer.shadowColor = UIColor.black.cgColor
+        summaryView.layer.shadowOpacity = 0.1
+        summaryView.layer.shadowOffset = CGSize(width: 0, height: -2)
+        summaryView.layer.shadowRadius = 6
+
+        summaryTitleLabel.text = "Order Summary"
+        summaryTitleLabel.font = AppFont.bold_18pt(size: 17)
+        summaryTitleLabel.textColor = AppColors.arsenicDark
+
+        totalTitleLabel.text = "Totals"
+        totalTitleLabel.font = AppFont.regular18pt(size: 16)
+        totalTitleLabel.textColor = AppColors.arsenicDark
+
+        totalValueLabel.text = "$0,00"
+        totalValueLabel.font = AppFont.bold_18pt(size: 17)
+        totalValueLabel.textColor = AppColors.arsenicDark
+
+        paymentButton.setTitle("Go to payment", for: .normal)
+        paymentButton.backgroundColor = AppColors.customBlue
+        paymentButton.titleLabel?.font = AppFont.bold_18pt(size: 17)
+        paymentButton.tintColor = .white
+        paymentButton.layer.cornerRadius = 12
+
+    }
+    
+    func setUpSummaryViewConstraints() {
+        [summaryTitleLabel, totalTitleLabel, totalValueLabel, paymentButton].forEach { summaryView.addSubview($0) }
+
+        summaryView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(150)
+        }
+
+        summaryTitleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(16)
+            make.leading.equalToSuperview().inset(20)
+        }
+
+        totalTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(summaryTitleLabel.snp.bottom).offset(12)
+            make.leading.equalTo(summaryTitleLabel)
+        }
+
+        totalValueLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(totalTitleLabel)
+            make.trailing.equalToSuperview().inset(20)
+        }
+
+        paymentButton.snp.makeConstraints { make in
+            make.top.equalTo(totalTitleLabel.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(48)
+        }
+    }
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
